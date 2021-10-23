5a1551c2bf4791210f51bf4c0e59e4a8cf0f2d1a
Macbook <macbook@waleed.local>
2021-10-23 17:55:00 +0300

    first page

---
+++ blog/blog/showVC.swift
@@ -0,0 +1,20 @@
+//
+//  showVC.swift
+//  blog
+//
+//  Created by Macbook on 17/03/1443 AH.
+//
+
+import UIKit
+
+class showVC : UIViewController{
+    
+    var selectedpro : Blogs?
+    
+   
+    var img1 = UIImage()
+    var name1 = ""
+    
+
+
+}
