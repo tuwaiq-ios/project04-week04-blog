a5b5ccb01e42bac21299d964f144f359cc0587a7
tasnim <tasneemjafsher@gmail.com>
2021-10-24 09:35:47 +0300

    Done

---
+++ BlogAppProject/BlogAppProject/HexColorExtension.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  BlogAppProject
+//
+//  Created by Tsnim Alqahtani on 17/03/1443 AH.
+//
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: String){
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        
+        switch hex.count {
+            
+        case 3:// RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17,(int & 0xF) * 17)
+            
+            case 6:// RGB (24-bit)
+                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+            
+            case 8:// RGB (32-bit)
+                (a, r, g, b) =  (int >> 24, int >> 4 & 0xFF , int >> 8 & 0xFF , int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: Double(a) / 255
+            )
+    }
+    }
+
+
+struct HexColorExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HexColorExtension_Previews: PreviewProvider {
+    static var previews: some View {
+        HexColorExtension()
+    }
+}
