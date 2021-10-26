c7b94b8bf31a65737a1cf6460ae13a9827eb2614
Jawaher <jawaher.hass@gmail.com>
2021-10-26 09:50:31 +0300

    done

--- BlogApp/BlogApp/SceneDelegate.swift
+++ BlogApp/BlogApp/SceneDelegate.swift
@@ -15,14 +15,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
      
-        if let windowScene = scene as? UIWindowScene {
-          let window = UIWindow(windowScene: windowScene)
-            
-            window.rootViewController = UIHostingController(rootView: Contentview())
-            
-          self.window = window
-          window.makeKeyAndVisible()
-        }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = navigationController
+        navigationController.navigationBar.prefersLargeTitles = true
+        window.makeKeyAndVisible()
+        self.window = window
         
         
     }
